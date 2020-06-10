0c09b9bfd11d63fd15c762698a2a2ee4c2b7852e
fkzack <github@fzack.com>
2020-06-10 08:05:00 -0700

    fix library call

--- centraldifference.r
+++ centraldifference.r
@@ -1,6 +1,11 @@
 # central diference approx to dx/dy, 
 # y is a date, we assume we are sorted in date order (ascending)
 # group_id identifies series that are in order, do not calculate across changes in group id
+
+library(lubridate)
+library(dplyr)
+
+
 centralDifference <- function (x,y, group_id){
   dx <- lead(x,1) - lag(x,1)
   dy <- lead(y,1) - lag(y,1)
